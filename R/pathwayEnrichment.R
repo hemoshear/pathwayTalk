09eef313f36adaaf861ea37db80c9a43030d5648
Justin Taylor <taylor@hemoshear.com>
2020-07-07 18:44:26 +0000

    changing pathway enrichment to use Entrez identifiers and reactome.db. Modified fisher.test call so that the alternative hypothesis is 'greater' instead of 'two.sided'

--- R/pathwayEnrichment.R
+++ R/pathwayEnrichment.R
@@ -1,65 +1,5 @@
 # pathwayEnrichment.R
 
-#' Get Reactome pathways and corresponding Hugo Gene Symbols
-#' for Homo sapiens
-#' 
-#' @param importFrom magrittr %<>%
-#' @param species Character vector to indicate which species should be used. Can be more than 1.
-#' @return list where each element is a vector of Hugo gene symbols that correspond to a Reactome pathway.
-#' @export 
-
-getReactomePathways <- function(species = 'Homo sapiens') {
-    # reactome_infile <- system.file('extdata', 'ReactomePathways.gmt', package = 'pathwayTalk')
-    # This file contains the Hugo gene symbols that correspond to each Reactome Pathway
-    reactome <- GSA::GSA.read.gmt('inst/extdata/ReactomePathways.txt')
-    # This file contains the reactome pathway identifiers, descriptions, and species.
-    pathways <- read.delim('https://reactome.org/download/current/ReactomePathways.txt',
-                           header=FALSE)
-    colnames(pathways) <- c('reaction_pathway', 'reactome_description', 'species')
-    # Filter pathways by species
-    pathways %<>% dplyr::filter(species %in% species)
-    # Build up a list of pathways
-    index <- purrr::map(pathways$reaction_pathway, 
-                        ~ which(reactome$geneset.descriptions == .))
-    names(index) <- paste0(pathways$reaction_pathway, '___', pathways$reactome_description)
-    index %<>% Filter(function(x) (length(x) > 0), .)
-    for (i in 1:length(index)) {
-        index[[i]] <- reactome$genesets[[index[[i]]]]
-    }
-    index
-}
-
-
-#' Link Entrez identifiers to Hugo Gene Symbols for the object
-#' produced by diffExpression
-#' 
-#' @importFrom magrittr %<>%
-#' @param deg Output of diffExpression, which is a list of topTables
-#'   for each contrast. 
-#' @return Original input is returned with two new columns: `entrezgene_id` and `hgnc_symbol`.
-#'   
-entrez_to_hgnc <- function(deg, mart='hsapiens_gene_ensembl') {
-    ensembl <- biomaRt::useMart('ensembl')
-    datasets <- biomaRt::listDatasets(ensembl)
-    hensembl <- biomaRt::useDataset(mart=ensembl, dataset=mart)
-    mapping <- biomaRt::getBM(attributes=c('hgnc_symbol', 'entrezgene_id'),
-                              mart=hensembl)
-    mapping %<>% dplyr::filter(!is.na(entrezgene_id), !is.na(hgnc_symbol),
-                               hgnc_symbol != "")
-    mapping %<>% dplyr::arrange(entrezgene_id)
-    ids <- purrr::map(deg, ~ rownames(.) %>%
-                          strsplit(split='///') %>%
-                          lapply(., function(x) (x[1])) %>%
-                          unlist)
-    mapping$entrezgene_id %<>% as.character()
-    for (i in 1:length(ids)) {
-        deg[[i]]$entrezgene_id <- ids[[i]]
-        deg[[i]] %<>% dplyr::inner_join(mapping)
-    }
-    deg
-}
-
-
 #' Do pathway enrichment with Fisher's exact test
 #' 
 #' @param deg diffExpression --> entrez_to_hgnc
@@ -73,22 +13,23 @@ entrez_to_hgnc <- function(deg, mart='hsapiens_gene_ensembl') {
 #' @export
 
 fisherPathwayEnrichment <- function(deg, alpha) {
-    pathways <- getReactomePathways()
+    pathways <- as.list(reactome.db::reactomePATHID2EXTID)
+    pathways <- pathways[grep('HSA', names(pathways))]
     sig <- deg %>% dplyr::filter(adj.P.Val < alpha)
     nonsig <- deg %>% dplyr::filter(adj.P.Val >= alpha)
     
     # Enumerate contingency table, this can be cleaned up a lot. 
     sig_in_pathway <- purrr::map(
-        pathways, ~ dplyr::filter(sig, hgnc_symbol %in% .) %>% nrow)
+        pathways, ~ dplyr::filter(sig, canon_entrez %in% .) %>% nrow)
     
     non_sig_in_pathway <- purrr::map(
-        pathways, ~ dplyr::filter(nonsig, hgnc_symbol %in% .) %>% nrow)
+        pathways, ~ dplyr::filter(nonsig, canon_entrez %in% .) %>% nrow)
     
     sig_not_in_pathway <- purrr::map(
-        pathways, ~ dplyr::filter(sig, !(hgnc_symbol %in% .)) %>% nrow)
+        pathways, ~ dplyr::filter(sig, !(canon_entrez %in% .)) %>% nrow)
     
     non_sig_not_in_pathway <- purrr::map(
-        pathways, ~ dplyr::filter(nonsig, !(hgnc_symbol %in% .)) %>% nrow)
+        pathways, ~ dplyr::filter(nonsig, !(canon_entrez %in% .)) %>% nrow)
     
     # Do Fisher's exact test for each reactome pathway.
     contingency <- fisher <- list()
@@ -98,10 +39,9 @@ fisherPathwayEnrichment <- function(deg, alpha) {
                                      sig_not_in_pathway[[i]],
                                      non_sig_not_in_pathway[[i]]),
                                    byrow = TRUE, nrow = 2, ncol=2)
-        res <- fisher.test(contingency[[i]])
+        res <- fisher.test(contingency[[i]], alternative = 'greater')
         # Change structure of fisher.test output to data frame
-        fisher[[i]] <- data.frame(p = res$p.value, conf_int_lb = res$conf.int[1],
-                         conf_int_ub = res$conf.int[2],
+        fisher[[i]] <- data.frame(p = res$p.value, 
                          estimate = res$estimate, pathway=names(pathways)[i])
     }
     dplyr::bind_rows(fisher) %>%
