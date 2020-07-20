d2f0efa19f39f976739dd25019a23f2b9f700a81
Justin Taylor <taylor@hemoshear.com>
2020-07-20 19:52:08 +0000

    Fixed pathway crosstalk function so it runs in a reasonable amount of time

--- R/pathwayCrosstalk.R
+++ R/pathwayCrosstalk.R
@@ -1,62 +1,10 @@
-#' Calculate matrix of discriminatory scores for pathway crosstalk
-#' 
-#' @param enriched_pathways
-#' @param eset 
-#' 
-
-pathwayCrosstalk <- function(enriched_pathways, eset) {
-    pathways <- as.list(reactome.db::reactomePATHID2EXTID)
-    pathways <- pathways[grep('HSA', names(pathways))]    # Convert Entrez to Hugo in data frame where we have probes collapsed to gene-level
-    d <- as.data.frame(genes$datETcollapsed)
-    d$entrez <- rownames(d)
-    d$canon_entrez <- strsplit(d$entrez, split = '///') %>%
-        purrr::map_chr(~ .[1])
-    # Get phenoData slot of the original ExpressionSet so we can map
-    # sample identifiers to treatments
-    pdata <- phenoData(eset)
-    pdata@data$sample_id <- rownames(pdata@data)# opinionated dplyr...
-    treatment_map <- pdata@data$title
-    names(treatment_map) <- pdata@data$sample_id
-    ds <- list()
-    for (i in 1:length(treatment_map)) { 
-        s <- list()
-        for (pathway in unique(enriched_pathways$pathway)) {
-            # Filter data frame with hugo gene symbols that co
-            this_d <- dplyr::filter(d, canon_entrez %in% pathways[[pathway]])
-            # Get gene expression values in the pathway
-            this_d <- this_d[c(names(treatment_map)[i])][[1]] %>% as.numeric
-            s[[pathway]] <- c('mean' = mean(this_d), 
-                        'sd' = sd(this_d))
-            
-        }
-        this_matrix <- matrix(NA, nrow=length(names(s)),
-                              ncol=length(names(s)))
-        rownames(this_matrix) <- colnames(this_matrix) <- names(s)
-        # Create matrix with discriminatory scores for the pathway pairs
-        for (n in 1:nrow(this_matrix)) {
-            for (m in 1:ncol(this_matrix)) {
-                if (m > n) {
-                    this_matrix[n,m] <- NA
-                }
-                pathway_n <- rownames(this_matrix)[n]
-                pathway_m <- colnames(this_matrix)[m]
-                this_matrix[n,m] <- (s[[pathway_n]]['mean'] - s[[pathway_m]]['mean']) / 
-                    (s[[pathway_n]]['sd'] + s[[pathway_m]]['sd'])
-            }
-        }
-        ds[[names(treatment_map)[i]]] <- this_matrix
-    }
-    ds
-}
-
-
 #' Calculate matrix of discriminating scores for pathway crosstalk
 #' 
 #' @param enriched_pathways
 #' @param eset 
 #' 
 
-pathwayCrosstalkParallel <- function(enriched_pathways, eset, processes=4) {
+pathwayCrosstalk <- function(enriched_pathways, eset, processes=1) {
     pathways <- as.list(reactome.db::reactomePATHID2EXTID)
     pathways <- pathways[grep('HSA', names(pathways))] # get pathways for homo sapiens
     # Get data frame of expression values
@@ -68,41 +16,32 @@ pathwayCrosstalkParallel <- function(enriched_pathways, eset, processes=4) {
     
     pdata <- phenoData(eset)
     pdata@data$sample_id <- rownames(pdata@data)
-    treatment_map <- pdata@data$title
-    names(treatment_map) <- pdata@data$sample_id
 
     do_pathway_crosstalk <- function(d, enriched_pathways) {
         s <- list()
         for (pathway in unique(enriched_pathways$pathway)) { 
             # Filter data frame to genes in pathway
             to_keep <- which(d$canon_entrez %in% pathways[[pathway]])
-            this_d <- d[to_keep,]
-            # Get gene expression values in the pathway
-            this_d <- this_d[[1]] %>% as.numeric
-            s[[pathway]] <- c('mean' = mean(this_d), 
-                              'sd' = sd(this_d))
+            s[[pathway]] <- c('mean' = mean(as.numeric(d[to_keep,1])), 
+                              'sd' = sd(as.numeric(d[to_keep,1])))
         }
-        this_matrix <- matrix(NA, nrow=length(names(s)),
-                              ncol=length(names(s)))
-        rownames(this_matrix) <- colnames(this_matrix) <- names(s)
-        # Create matrix with discriminating scores for the pathway pairs
-        for (n in 1:nrow(this_matrix)) {
-            for (m in 1:ncol(this_matrix)) {
-                if (m > n) {
-                    this_matrix[n,m] <- NA
-                }
-                pathway_n <- rownames(this_matrix)[n]
-                pathway_m <- colnames(this_matrix)[m]
-                this_matrix[n,m] <- (s[[pathway_n]]['mean'] - s[[pathway_m]]['mean']) / 
-                    (s[[pathway_n]]['sd'] + s[[pathway_m]]['sd'])
-            }
-        }
-        return(this_matrix)
+        pathway_pairs <- gtools::combinations(n=length(unique(enriched_pathways$pathway)),
+                                              r=2, v=unique(enriched_pathways$pathway))
+        scores <- purrr::map2(as.character(pathway_pairs[,1]), 
+                              as.character(pathway_pairs[,2]),
+                              ~ (s[[.x]]['mean'] - s[[.y]]['mean']) /
+                                  (s[[.x]]['sd'] + s[[.y]]['sd']))
+        names(scores) <- apply(pathway_pairs, 1, function(x) (paste0(x[1], '___', x[2], collapse='___')))
+        return(scores)
     }
     
-    sample_data <- purrr::map(names(treatment_map), ~ d[,c(., 'entrez', 'canon_entrez')])
+    sample_data <- purrr::map(pdata@data$sample_id, ~ d[,c(., 'entrez', 'canon_entrez')])
     res <- parallel::mclapply(sample_data, function(x) (do_pathway_crosstalk(x, enriched_pathways)),
                               mc.cores=processes)
-    names(res) <- names(treatment_map)
-    return(res)
+    pathway_pairs <- names(res[[1]]) # get pathway pairs for first sample, which will be same as the rest
+    res <- purrr::map(res, ~ unlist(.))
+    m <- do.call('cbind', res)
+    colnames(m) <- pdata@data$sample_id
+    rownames(m) <- pathway_pairs
+    return(m)
 }
